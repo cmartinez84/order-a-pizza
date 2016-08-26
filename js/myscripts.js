fea3301810c4f61cd2f1942237aeedeea349206c
epicodus <epicodus@BOGAs-MacBook-Pro.local>
2016-08-26 09:11:54 -0700

    create pizza empty pizza ordering oject, individual pizza constructor, and pizza price constructor

---
+++ js/myscripts.js
@@ -0,0 +1,14 @@
+function Pizza (size, toppings, crust){
+  this.size = size;
+  this.toppings = toppings;
+  this.crust = curst;
+}
+function PriceConstructor (modification, price){
+  this.modification = modification;
+  this.price = price;
+}
+var orderForm = {
+  this. sizeArray = [];
+  this.toppingsArray = [];
+  this.crustOptions = []
+}
