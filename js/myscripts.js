8631b48e76e5dfbfaea7e178bfe36ea59909e68e
epicodus <epicodus@BOGAs-MacBook-Pro.local>
2016-08-26 14:36:26 -0700

    add descriptions to all types of pizza options as well as displaying price in div

--- js/myscripts.js
+++ js/myscripts.js
@@ -24,7 +24,7 @@ var orderForm = {
 }
 
 var sizes = [["personal pan pizza",1], ["small", 2],["medium", 3],["large", 4],["x-large", 5],["family size", 6]];
-var toppings = [["anchovies", 1],["pepperoni", 1],["genoa salami",1.5 ],["chorizo", 1],["bell peppers", 1],["tomatoes", .75],["olives",.50],["buffalo mozzerella", 1.5],["fresh basil", 1]];
+var toppings = [["anchovies", 1],["pepperoni", 1],["genoa salami",1.5 ],["chorizo", 1],["bell peppers", 1],["tomatoes", .75],["olives",.50],["buffalo mozzerella", 2],["fresh basil", 1]];
 var crusts = [["thin curst", 1],["regular curst", 1],["stuffed crust", 2],["double decker", 3 ], ["gluten-free", 4]];
 
 sizes.forEach(function(sizePair){
