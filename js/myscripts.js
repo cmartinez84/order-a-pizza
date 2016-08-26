e71b47d9f8ec8526a141f8ef3976146168967f7e
epicodus <epicodus@BOGAs-MacBook-Pro.local>
2016-08-26 09:41:36 -0700

    price constructor name chnges to OptionPrice, size array created and pushed as series of objects to be stored in orderForm

--- js/myscripts.js
+++ js/myscripts.js
@@ -3,7 +3,7 @@ function Pizza (size, toppings, crust){
   this.toppings = toppings;
   this.crust = curst;
 }
-function PriceConstructor (modification, price){
+function OptionPrice (modification, price){
   this.modification = modification;
   this.price = price;
 }
@@ -13,3 +13,10 @@ var orderForm = {
   toppingsArray: [],
   crustOptions: []
 }
+var sizes = [["personal pan pizza",1], ["small", 2],["medium", 3],["large", 4],["x-large", 5],["family size", 6]];
+sizes.forEach(function(sizePair){
+  var newPrice = new OptionPrice(sizePair[0], sizePair[1]);
+  orderForm.sizeArray.push(newPrice);
+  console.log(newPrice);
+
+  });
